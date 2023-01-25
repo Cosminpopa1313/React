40c9196d072425b89ebb3067188b4a805ec87202
Cosmin <cosmin.p420@gmail.com>
2023-01-25 22:03:07 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
+
+
+
+
+class Arrow extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      direction:props.direction,
+      value:props.value
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+
+  handleClick(e){
+    a
+    document.getElementById("text").innerHTML = this.state.value
+  }
+
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleClick}>{this.state.value}</button>
+
+      </div>
+    ) 
+  }
+
+}
+
+
+
+
+
+
+class Controller extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {
+
+      }
+    }
+
+    render(){
+
+      // Direction           up -> 0          down -> 1
+
+
+      return(
+        <div>
+          <Arrow direction={0} value="UP"></Arrow>
+          <Arrow direction={1} value="DOWN"></Arrow>
+
+        </div>
+        )
+    }
+
+
+}
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Controller></Controller>
+      <a id="text"></a>
     </div>
   );
 }
+
+
+
+
 
 export default App;
